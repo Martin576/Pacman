9b166a88a0d9a9f80b6544c8d12b3c8975bacd8f
Martin <m.matignon@eleves-alsacienne.org>
2020-05-06 18:40:35 +0200

    il est de nouveau à jour c'est encore plus cool

--- projet.py
+++ projet.py
@@ -1,4 +1,4 @@
-print(42)
+# print(42)
 
 # labyrinthe stable 10x10
 #étape 1 
@@ -80,41 +80,41 @@ def creerLabyrinthe6x6():
   laby = [[]] * (7)
   for i in range (0, 7):
     laby[i] = [0] * (7)
-  laby[0][0] = [True, True]
-  laby[0][6] = [True, False]
+  laby[0][0] = [True, True, False]
+  laby[0][6] = [True, False, False]
   for i in range (1, 6):
-    laby[0][i] = [False, True]
+    laby[0][i] = [False, True, False]
   for j in range (1, 6):
-    laby[j][0] = [True, False]
-    laby[j][6] = [True, False]
+    laby[j][0] = [True, False, False]
+    laby[j][6] = [True, False, False]
     for k in range (1, 6):
-      laby[j][k] = [False, False]
-  laby[6][0] = [False, True]
-  laby[6][6] = [False, False]
+      laby[j][k] = [False, False, False]
+  laby[6][0] = [False, True, False]
+  laby[6][6] = [False, False, False]
   for m in range (1, 6):
-    laby[6][m] = [False,True]
-  laby[0][1] = [True, True]
-  laby[1][1] = [False, True]
-  laby[1][2] = [False, True]
-  laby[1][3] = [False, True]
-  laby[1][4] = [True, False]
-  laby[1][5] = [True, False]
-  laby[2][1] = [False, True]
-  laby[2][2] = [True, True]
-  laby[2][5] = [False, True]
-  laby[3][0] = [True, True]
-  laby[3][1] = [True, False]
-  laby[3][2] = [True, False]
-  laby[3][3] = [False, True]
-  laby[3][4] = [True, False]
-  laby[3][5] = [False, True]
-  laby[4][1] = [True, False]
-  laby[4][2] = [False, True]
-  laby[4][3] = [False, True]
-  laby[4][5] = [True, False]
-  laby[5][1] = [False, True]
-  laby[5][3] = [False, True]
-  laby[5][4] = [True, True]
+    laby[6][m] = [False,True,False]
+  laby[0][1] = [True, True,False]
+  laby[1][1] = [False, True, False]
+  laby[1][2] = [False, True, False]
+  laby[1][3] = [False, True, False]
+  laby[1][4] = [True, False, False]
+  laby[1][5] = [True, False, False]
+  laby[2][1] = [False, True, False]
+  laby[2][2] = [True, True, False]
+  laby[2][5] = [False, True, False]
+  laby[3][0] = [True, True, False]
+  laby[3][1] = [True, False, False]
+  laby[3][2] = [True, False, False]
+  laby[3][3] = [False, True, False]
+  laby[3][4] = [True, False, False]
+  laby[3][5] = [False, True, False]
+  laby[4][1] = [True, False, False]
+  laby[4][2] = [False, True, True]
+  laby[4][3] = [False, True, False]
+  laby[4][5] = [True, False, False]
+  laby[5][1] = [False, True, False]
+  laby[5][3] = [False, True, False]
+  laby[5][4] = [True, True, False]
   return(laby)
 
 # Fonction : hauteurLabyrinthe
@@ -127,6 +127,10 @@ def largeurLabyrinthe(laby):
   l = len(laby[0]) - 1
   return(l)
 
+def afficherPointMurAGauche():
+  return("| • ")
+def afficherPointSansMur():
+  return("  • ")
 def afficherUnMurAuDessus():
   return("+---")
 def afficherPasDeMurAuDessus():
@@ -150,10 +154,14 @@ def afficherUneLigneMilieu(t):
   l = len(t)
   u = [0] * (l + 1)
   for i in range (0, l):
-    if (t[i][0] == True):
+    if (t[i][0] == True) and (t[i][2]==True):
+      u[i] = afficherPointMurAGauche()
+    if (t[i][0] == True) and (t[i][2]==False):
       u[i] = afficherUnMurAGauche()
-    else:
+    if (t[i][0] == False) and (t[i][2]==False):
       u[i] = afficherPasDeMurAGauche()
+    if (t[i][0] == False) and (t[i][2]==True):
+      u[i] = afficher()
   for j in range (0, l):
     print(u[j], end="")
   print()
@@ -163,7 +171,46 @@ def afficherLabyrinthe(laby):
     afficherUneLigneDuHaut(laby[i])
     afficherUneLigneMilieu(laby[i])
 
-laby1 = creerLabyrinthe10x10()
-afficherLabyrinthe(laby1)
-laby2 = creerLabyrinthe6x6()
-afficherLabyrinthe(laby2)
+# laby1 = creerLabyrinthe10x10()
+# afficherLabyrinthe(laby1)
+# laby2 = creerLabyrinthe6x6()
+# afficherLabyrinthe(laby2)
+
+
+class Partie:
+
+  def __init__(self):
+    self.labyrinthe = creerLabyrinthe10x10()
+    self.positionDuJoueur = (0, 9)
+
+  def afficherPositionJoueur(self):
+    print("Position du joueur :", self.positionDuJoueur)
+
+  def deplacerJoueurVersLaDroite(self):
+    (i, j) = self.positionDuJoueur
+    if (self.peutSeDeplacerVersLaDroite(i, j)):
+      self.positionDuJoueur = (i, j+1)
+    else:
+      pass
+
+  def peutSeDeplacerVersLaDroite(self, iActuel, jActuel):
+    return self.labyrinthe[iActuel][jActuel][0] == True
+
+  def afficher(self):
+    # Ecrire une belle fonction qui utilise self.labyrinthe et self.positionDuJoueur
+    pass
+
+partie = Partie()
+partie.afficherPositionJoueur()
+partie.deplacerJoueurVersLaDroite()
+partie.afficherPositionJoueur()
+
+def effectuerAction(partie, action):
+  if (action == "deplacementVersLaDroite"):
+    partie.deplacerJoueurVersLaDroite()
+
+
+while(True):
+  prochaineAction = recupererProchaineAction() # Fonction basée sur input()
+  effectuerAction(partie, prochaineAction)
+  partie.afficher()
